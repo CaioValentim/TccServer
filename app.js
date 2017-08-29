4d229d34338bc4441118f4946b039c2488a4d7d5
CaioValentim <caioval7@hotmail.com>
2017-08-29 19:25:15 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+﻿var express = require('express');
+var app = express();
+var path = require("path");
+var models = require("./models");
+var cors = require('cors');
+var usersRouters = require("./routers/usuario");
+var loginRouters = require("./routers/login");
+var passport = require('passport')
+app.use(passport.initialize());
+app.use(function allowCrossOrigin(req, res, next){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEADERS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        }
+        else {
+            next();
+        }
+    });
+app.use(passport.initialize());
+app.use(usersRouters);
+app.use(loginRouters);
+
+
+models.sequelize.sync().then(function(){
+  app.listen(8080,function(){
+  	console.log("Executando.");
+  });
+});
