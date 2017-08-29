e11bcbb8577be1d62fcec4bd009dc9e3e7988b4e
CaioValentim <caioval7@hotmail.com>
2017-08-29 19:25:37 -0300

    Add files via upload

---
+++ config/dataBase.js
@@ -0,0 +1,12 @@
+var Sequelize = require("sequelize");
+const sequelize = new Sequelize('teste', 'root', 'dharok2', {
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+module.exports =  sequelize;
